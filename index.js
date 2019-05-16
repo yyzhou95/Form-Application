ea0a7ddc199a4e19f47524151f9683a852255ff9
Null <borismirage00@gmail.com>
2019-05-15 20:08:39 -0700

    Test

--- index.js
+++ index.js
@@ -17,12 +17,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 // set ejs as default file
 app.set("view engine", "ejs");
 
+// post request handler
 app.post("/newpost", function (req, res) {
     let content = req.body.content;
     arr.push(content);
-    res.redirect("/");
-
-    // res.send(req.body);
+    res.redirect("/");      // redirect to home page
 });
 
 // root path
@@ -31,7 +30,6 @@ app.get('/', function (req, res) {
     res.render('home', {arr: arr});
 });
 
-
 // test parameter passing through
 app.get('/para/:para', function (req, res) {
     let pass = req.params.para;
@@ -39,7 +37,6 @@ app.get('/para/:para', function (req, res) {
     console.log("Request " + pass);
     res.render('para', {para: pass});
 });
-
 
 // loop test
 app.get('/loop/:iter', function (req, res) {
@@ -50,7 +47,6 @@ app.get('/loop/:iter', function (req, res) {
     ];
     res.render('loop', {arr: array})
 });
-
 
 // finally, if a request is not recorded in router, return a 404 page
 app.get('*', function (req, res) {
