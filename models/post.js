f28013db74c44d493110c16de430fa2421dad20e
Null <borismirage00@gmail.com>
2019-05-27 15:56:42 -0700

    Add attribute to post

--- models/post.js
+++ models/post.js
@@ -3,7 +3,10 @@ let mongoose = require('mongoose');
 /* schema setup */
 let postSchema = new mongoose.Schema({
     name: String,
-    link: String,
+    link: {
+        type: String,
+        default: ''
+    },
     postContent: String,
     uploadUser: {
         id: {
@@ -17,7 +20,11 @@ let postSchema = new mongoose.Schema({
             type: mongoose.Schema.Types.ObjectId,
             ref: "CommentModel"      // name of the model, should be identical to the export model in comment.js
         }
-    ]
+    ],
+    createDate: {
+        type: Date,
+        default: Date.now()
+    }
 });
 
 /* export database model that is set up via schema */
